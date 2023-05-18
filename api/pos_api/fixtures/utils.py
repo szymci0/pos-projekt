5d1f5f1c2b57d4974394c2b5d3788ea5ed262773
szymci0 <szymonbetk@gmail.com>
2023-05-18 23:51:47 +0200

    Add injecting positions fixtures

--- api/pos_api/fixtures/utils.py
+++ api/pos_api/fixtures/utils.py
@@ -6,6 +6,7 @@ from pkg_resources import resource_filename
 from pos_api.models import db
 from pos_api.app import create_app
 from pos_api.models.users import UserCreate
+from pos_api.models.position import Position
 
 from pos_pkg.files.utils import read_from_json
 from pos_pkg.config import MONGODB_DB_NAME
@@ -28,5 +29,13 @@ async def inject_users():
             ...
     print(f"Injected {len(users)} users")
 
+
+async def inject_positions():
+    positions = read_from_json(Path(resource_filename(__name__, "positions.json")))
+    for pos in positions:
+        Position(**pos).save()
+    print(f"Injected {len(positions)} positions")
+
 async def inject_fixtures():
     await inject_users()
+    await inject_positions()
