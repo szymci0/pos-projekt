f095ce3d3cf26d93ec8fe794b915f4768e653cbf
szymci0 <szymonbetk@gmail.com>
2023-06-10 02:18:22 +0200

    Install v-tables and display one

--- web/src/views/UsersView.vue
+++ web/src/views/UsersView.vue
@@ -1,8 +1,32 @@
 <template>
-    <div>WIP</div>
+    <div class="d-flex justify-content-center align-items-cente">
+        <v-client-table :data="data.tableData" :columns="data.columns">
+
+        </v-client-table>
+    </div>
 </template>
 <script>
 export default {
     name: "UsersView",
+    mounted() {
+
+    },
+    data() {
+        return {
+            data: {
+                columns: ['id', 'name', 'age'],
+                tableData: [
+                    { id: 1, name: "John", age: "20" },
+                    { id: 2, name: "Jane", age: "24" },
+                    { id: 3, name: "Susan", age: "16" },
+                    { id: 4, name: "Chris", age: "55" },
+                    { id: 5, name: "Dan", age: "40" }
+                ],
+                options: {
+                    // see the options API
+                }
+            }
+        }
+    }
 }
 </script>
